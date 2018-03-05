63a7d5daa9d0798e8ea5217b8a572555e3867ad3
Joel Wurtz <jwurtz@jolicode.com>
2018-03-05 18:36:24 +0100

    Send scheme in logs and redirection

--- src/HttpMessage/Request.php
+++ src/HttpMessage/Request.php
@@ -8,6 +8,7 @@ class Request
     private $path;
     private $userAgent;
     private $referer;
+    private $scheme;
 
     /**
      * @param string $host      Host of the URI instance
@@ -15,12 +16,13 @@ class Request
      * @param string $userAgent User-Agent header of the request
      * @param string $referer   Referer header of the request
      */
-    public function __construct($host, $path, $userAgent, $referer = '')
+    public function __construct($host, $path, $userAgent, $referer = '', $scheme = 'http')
     {
         $this->host = $host;
         $this->path = $path;
         $this->userAgent = $userAgent;
         $this->referer = $referer;
+        $this->scheme = $scheme;
     }
 
     public function getHost()
@@ -41,5 +43,10 @@ class Request
     public function getReferer()
     {
         return $this->referer;
+    }
+
+    public function getScheme()
+    {
+        return $this->scheme;
     }
 }
